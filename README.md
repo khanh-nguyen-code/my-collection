9a5d5a90d072498c2affeb70f01601fb88722275
khanh.nguyen <khanh.nguyen@shopee.com>
2021-12-03 20:22:21 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # python_util
+
+collection of utility libraries for python
+
+# packaging
+
+```bash
+rm -rf dist/*
+python setup.py sdist bdist_wheel
+twine upload dist/*
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="khanh-python-util",
+    version="0.0.1",
+    author="Nguyen Ngoc Khanh",
+    author_email="khanh.nguyen.contact@gmail.com",
+    description="collection of utility libraries for python",
+    url="https://github.com/khanh-nguyen-code/python_util",
+    packages=setuptools.find_packages(),
+)
