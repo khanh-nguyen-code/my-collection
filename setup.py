21ef80e27d40caa33194bb38208fed9b062dc36c
khanh.nguyen <khanh.nguyen@shopee.com>
2021-12-06 11:51:12 +0800

    commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="my-collection",
-    version="0.0.7",
+    version="0.0.8",
     author="Nguyen Ngoc Khanh",
     author_email="khanh.nguyen.contact@gmail.com",
     description="collection of utility libraries for python",
