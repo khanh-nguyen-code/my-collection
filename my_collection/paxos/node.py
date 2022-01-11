f093c1f66298db348d9cb2aeb0a3ea82dfc6bdad
khanh.nguyen <khanh.nguyen@shopee.com>
2022-01-11 13:01:35 +0800

    commit

--- my_collection/paxos/node.py
+++ my_collection/paxos/node.py
@@ -1,12 +1,10 @@
 import asyncio
-import json
 import threading
-from collections import namedtuple
 from typing import Optional
 
 import aiohttp
 import fastapi
-import uvicorn as uvicorn
+import uvicorn
 
 from my_collection import http
 from my_collection.paxos.acceptor import Acceptor
@@ -17,108 +15,75 @@ from my_collection.paxos.proposer import Proposer
 
 router = http.Router()
 
-Version = namedtuple("Version", ["proposer", "acceptor", "learner"])
-
 
 class Node(http.Server):
-    node_id: NodeId
+    proposer: Proposer
+    acceptor: Acceptor
+    learner: Learner
     addr_list: list[str]
-    log: list[Version]
-    line: int
 
     def __init__(self, node_id: NodeId, addr_list: list[str]):
         super().__init__(router)
-        self.node_id = node_id
         self.addr_list = addr_list
-        self.log = []
-        self.line = 0
+        self.proposer = Proposer(node_id, list(range(len(addr_list))), self.route)
+        self.acceptor = Acceptor(node_id, list(range(len(addr_list))), self.route)
+        self.learner = Learner(node_id, len(addr_list))
 
-    def extend_log(self):
-        log_id = len(self.log)
-        self.log.append(Version(
-            proposer=Proposer(self.node_id, list(range(len(addr_list))), self.route(log_id)),
-            acceptor=Acceptor(self.node_id, list(range(len(addr_list))), self.route(log_id)),
-            learner=Learner(self.node_id, len(addr_list)),
-        ))
+    async def route(self, node_id: NodeId, request: Request, unmarshaller: Unmarshaller) -> Optional[Response]:
+        path = self.addr_list[node_id]
+        if isinstance(request, PrepareRequest):
+            path += "/internal/prepare"
+        if isinstance(request, ProposeRequest):
+            path += "/internal/propose"
+        if isinstance(request, LogRequest):
+            path += "/internal/log"
 
-    def route(self, log_id: int):
-        async def helper(node_id: NodeId, request: Request, unmarshaller: Unmarshaller) -> Optional[Response]:
-            path = self.addr_list[node_id]
-            if isinstance(request, PrepareRequest):
-                path += "/internal/prepare"
-            if isinstance(request, ProposeRequest):
-                path += "/internal/propose"
-            if isinstance(request, LogRequest):
-                path += "/internal/log"
-            path += f"/{log_id}"
-            async with aiohttp.ClientSession() as session:
-                async with session.post(path, json=request.dict()) as r:
-                    if r.status != 200:
-                        return None
-                    o = await r.json()
-                    if o is None:
-                        return None
-                    if unmarshaller is None:
-                        return None
-                    response = unmarshaller(o)
-                    if response.code != CODE_OK:
-                        print(response)
-                    return response
+        async with aiohttp.ClientSession() as session:
+            async with session.post(path, json=request.dict()) as r:
+                if r.status != 200:
+                    return None
+                o = await r.json()
+                if o is None:
+                    return None
+                if unmarshaller is None:
+                    return None
+                response = unmarshaller(o)
+                if response.code != CODE_OK:
+                    print(response)
+                return response
 
-        return helper
+    @router.http_method(router.method_post, "/internal/log")
+    async def internal_log(self, body: LogRequest = fastapi.Body(default=None)) -> None:
+        return await self.learner.handle_log_request(body)
 
-    @router.http_method(router.method_post, "/internal/log/{log_id}")
-    async def internal_log(self, log_id: int = fastapi.Path(default=None),
-                           body: LogRequest = fastapi.Body(default=None)) -> None:
-        while log_id >= len(self.log):
-            self.extend_log()
-        return await self.log[log_id].learner.handle_log_request(body)
+    @router.http_method(router.method_post, "/internal/prepare")
+    async def internal_prepare(self, body: PrepareRequest = fastapi.Body(default=None)) -> Optional[PrepareResponse]:
+        return await self.acceptor.handle_prepare_request(body)
 
-    @router.http_method(router.method_post, "/internal/prepare/{log_id}")
-    async def internal_prepare(self, log_id: int = fastapi.Path(default=None),
-                               body: PrepareRequest = fastapi.Body(default=None)) -> Optional[PrepareResponse]:
-        while log_id >= len(self.log):
-            self.extend_log()
-        return await self.log[log_id].acceptor.handle_prepare_request(body)
-
-    @router.http_method(router.method_post, "/internal/propose/{log_id}")
-    async def internal_propose(self, log_id: int = fastapi.Path(default=None),
-                               body: ProposeRequest = fastapi.Body(default=None)) -> Optional[ProposeResponse]:
-        while log_id >= len(self.log):
-            self.extend_log()
-        return await self.log[log_id].acceptor.handle_propose_request(body)
+    @router.http_method(router.method_post, "/internal/propose")
+    async def internal_propose(self, body: ProposeRequest = fastapi.Body(default=None)) -> Optional[ProposeResponse]:
+        return await self.acceptor.handle_propose_request(body)
 
     @router.http_method(router.method_post, "/propose")
-    async def propose(self, inValue: str = fastapi.Query(default=None)) -> list[str]:
-        value = json.dumps({
-            "node_id": self.node_id,
-            "line": self.line,
-            "value": inValue},
-        )
-        while True:
-            self.extend_log()
-            log_id = len(self.log) - 1
-            if self.log[log_id].learner.committed is None:
-                wait = 0.15  # s
-                max_wait = 2  # s
-                while True:
-                    if self.log[log_id].learner.committed is not None:
-                        break
-                    value = await self.log[log_id].proposer.propose_once(value)
-                    if value is not None:
-                        self.log[log_id].learner.committed = value
-                        self.log[log_id].learner.received = None
-                        break
-                    if self.log[log_id].learner.committed is not None:
-                        break
-                    await asyncio.sleep(wait)
-                    wait *= 2
-                    if wait > max_wait:
-                        wait = max_wait
-            if self.log[log_id].learner.committed == value:
-                break
-        self.line += 1
-        return [log.learner.committed for log in self.log]
+    async def propose(self, value: Value = fastapi.Query(default=None)) -> Optional[Value]:
+        if self.learner.committed is None:
+            wait = 0.15  # s
+            max_wait = 2  # s
+            while True:
+                if self.learner.committed is not None:
+                    break
+                value = await self.proposer.propose_once(value)
+                if value is not None:
+                    self.learner.committed = value
+                    self.learner.received = None
+                    break
+                if self.learner.committed is not None:
+                    break
+                await asyncio.sleep(wait)
+                wait *= 2
+                if wait > max_wait:
+                    wait = max_wait
+        return self.learner.committed
 
     @router.http_method(router.method_get, "/committed")
     async def committed(self) -> Value:
